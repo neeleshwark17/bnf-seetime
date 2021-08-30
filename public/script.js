552908329a27ccb58ff23350a08561f51a86146e
neeleshwarRubex <neeleshwark17@gmail.com>
2021-08-30 16:45:39 +0530

    2.8

--- public/script.js
+++ public/script.js
@@ -1,15 +1,15 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
-// const myPeer = new Peer(undefined, {
-//   host: "/",
-//   port: "3001",
-// });
-
-const myPeer = new Peer({
-  secure: true,
-  host: "bnf-seetime.herokuapp.com",
-  port: 443,
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
 });
+
+// const myPeer = new Peer(undefined,{
+//   secure: true,
+//   host: "bnf-seetime.herokuapp.com",
+//   port: 443,
+// });
 
 const myVideo = document.createElement("video");
 myVideo.muted = true;
