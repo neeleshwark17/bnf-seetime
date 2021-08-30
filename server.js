1deec7a90aa884d06bf1c489f37289aee36d39a6
neeleshwarRubex <neeleshwark17@gmail.com>
2021-08-30 15:31:56 +0530

    1.0

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const socket = require("socket.io");
+const { v4: uuidV4 } = require("uuid");
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log("server running on -", PORT);
+});
+
+var io = socket(server);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+    socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
