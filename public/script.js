e6c3a999d090694b8f26b99737b7653ac67fdab2
neeleshwarRubex <neeleshwark17@gmail.com>
2021-08-30 15:56:29 +0530

    1.8

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   // host: "/",
   // port: "3001",
-  host: "bnf-seetime.herokuapp.com/",
+  host: "https://bnf-seetime.herokuapp.com/",
   secure: "true",
   port: "443",
 });
